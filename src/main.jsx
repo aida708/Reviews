4c16c27d325c48043e0d58fbc47c25ad6b3e5330
Aida <aidasufaj00@gmail.com>
2023-11-05 12:50:54 +0100

    testing the alternative

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Alternative from "./Alternative";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Alternative />
+  </React.StrictMode>
+);

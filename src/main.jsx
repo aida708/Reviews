561810a90fa2b0a5b08254a88694ce166fd60b89
Aida <aidasufaj00@gmail.com>
2023-11-05 12:51:37 +0100

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import Alternative from "./Alternative";
+// import Alternative from "./Alternative";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Alternative />
+    <App />
   </React.StrictMode>
 );
